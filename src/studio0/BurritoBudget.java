a5bc8dc51084e41ecfb75407831465c51480835f
annecrum <annecrum@localhost>
2024-08-28 14:02:45 -0500

    Signed-off-by: annecrum <annecrum@localhost>

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,11 +8,12 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
 		
-		double burritoPrice = 7.50;
-		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
+		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		double averageWeeklyBurritos = totalBurritos / 4.0;
+		
+		double burritoprice = 7.50;
+		double averageWeeklyPrice = averageWeeklyBurritos * burritoprice;
 		
 		System.out.println("You have spent an average of $" + averageWeeklyPrice + " per week on burritos.");
 	
